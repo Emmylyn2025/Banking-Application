0abd0c26dd06721b155a8102785d2a9f4c4fc790
Awobodu Emmanuel <awoboduemmanuel@gmail.com>
2026-04-27 15:31:54 +0100

    Configured redis well

--- src/Redis/redis.ts
+++ src/Redis/redis.ts
@@ -2,7 +2,21 @@ import { Redis } from "ioredis";
 
 const redis = new Redis({
   host: "localhost",
-  port: 6379
+  port: 6379,
+  maxRetriesPerRequest: 3
+});
+
+redis.on("connect", () => {
+  console.log("Connected to redis");
+});
+
+redis.on("error", (err: Error) => {
+  console.error("Redis error:", err);
+});
+
+// Event: Reconnecting
+redis.on("reconnecting", (time: number) => {
+  console.warn(`Redis: Reconnecting in ${time}ms...`);
 });
 
 export default redis;
