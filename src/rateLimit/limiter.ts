41881b49d661d05034ff130bcc4c81023cc32ccc
Awobodu Emmanuel <awoboduemmanuel@gmail.com>
2026-05-05 21:00:33 +0100

    I created a auth limiter to protect the authenticated routes

--- src/rateLimit/limiter.ts
+++ src/rateLimit/limiter.ts
@@ -2,10 +2,10 @@ import { rateLimit } from "express-rate-limit";
 import { RedisStore, SendCommandFn } from "rate-limit-redis";
 import redis from "../Redis/redis";
 
-const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 10, // Limit each IP to 10 requests per `window` (here, per 5 minutes)
-  message: "Too many requests from this IP, please try again after 5 minutes",
+export const limiterAuth = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 mins
+  max: 10, // much lower
+  message: "Too many auth attempts, try again later",
   standardHeaders: "draft-7",
   legacyHeaders: false,
   store: new RedisStore({
@@ -16,4 +16,17 @@ const limiter = rateLimit({
   })
 });
 
-export default limiter;
+
+export const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
+  message: "Too many requests from this IP, please try again after 5 minutes",
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+  store: new RedisStore({
+    sendCommand: (async (...args: string[]) => {
+      const [command, ...rest] = args;
+      return await redis.call(command, ...rest);
+    }) as SendCommandFn
+  })
+});
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -5,27 +5,28 @@ import { auth, adminAuth } from "../middleware/authMiddleware";
 import validateRequest from "../zod/validationMiddleware";
 import { registerSchema, LoginSchema, forgotPasswordSchema, resetPasswordSchema } from "../zod/userShema";
 import transferSchema from "../zod/transferSchema";
+import { limiterAuth, limiter } from "../rateLimit/limiter";
 
 
 const router = express.Router();
 
 //User routes
-router.post('/register', validateRequest(registerSchema), registerUser);
-router.post('/login', validateRequest(LoginSchema), loginUser);
-router.post('/refresh', refresh);
+router.post('/register', limiterAuth, validateRequest(registerSchema), registerUser);
+router.post('/login', limiterAuth, validateRequest(LoginSchema), loginUser);
+router.post('/refresh', limiter, refresh);
 router.post('/logout', logout);
-router.get('/users', auth, adminAuth, allUsers);
-router.get("/my-account", auth, myAccount);
-router.patch('/users/:userId', auth, adminAuth, updateUser);
-router.delete('/users/:userId', auth, adminAuth, deleteUser);
-router.post('/forgot-password', validateRequest(forgotPasswordSchema), forgotPassword);
-router.post('/reset-password/:token', validateRequest(resetPasswordSchema), resetPassword);
-router.get('/users/:id', auth, adminAuth, userById);
+router.get('/users', limiter, auth, adminAuth, allUsers);
+router.get("/my-account", limiter, auth, myAccount);
+router.patch('/users/:userId', limiter, auth, adminAuth, updateUser);
+router.delete('/users/:userId', limiter, auth, adminAuth, deleteUser);
+router.post('/forgot-password', limiterAuth, validateRequest(forgotPasswordSchema), forgotPassword);
+router.post('/reset-password/:token', limiterAuth, validateRequest(resetPasswordSchema), resetPassword);
+router.get('/users/:id', limiter, auth, adminAuth, userById);
 
 //Account routes
-router.get('/account', auth, getAccount);
-router.post('/transfer', auth, validateRequest(transferSchema), transfer);
-router.get('/transfer', auth, allTransactions);
-router.get('/transfer/:id', auth, getTransactionById);
+router.get('/account', limiter, auth, getAccount);
+router.post('/transfer', limiterAuth, auth, validateRequest(transferSchema), transfer);
+router.get('/transfer', limiter, auth, allTransactions);
+router.get('/transfer/:id', limiter, auth, getTransactionById);
 
 export default router;
